5709e8d586f895bea4f72204b0d1c72dd50293ca
Javier <test@test.gov>
2015-11-30 16:18:50 -0400

    Adding a second image

--- one/script.js
+++ one/script.js
@@ -1,7 +1,18 @@
 var cat = document.getElementById('click');
 var counts = document.getElementById('counts');
+
+var cat2 = document.getElementById('click2');
+var counts2 = document.getElementById('counts2');
+
 var i=0;
+var j=0;
+
 cat.addEventListener('click',function(){
 	i++;
 	counts.innerHTML=i;
+});
+
+cat2.addEventListener('click',function(){
+	j++;
+	counts2.innerHTML = j;
 });
